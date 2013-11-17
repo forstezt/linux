d09acdda420c55ed3a7b22ec5a3e508b4eea9e05
Zach <zforster@hotmail.com>
2013-11-17 16:45:34 -0600

    Edited .vimrc to use 4 spaces instead of tabs.

--- .vimrc
+++ .vimrc
@@ -1,1 +1,8 @@
+"allow users to use the mouse in vim
 set mouse=a
+
+"make tabs and indents 4 spaces
+set expandtab
+set tabstop=4
+retab
+set shiftwidth=4
--- mint_init_setup.sh
+++ mint_init_setup.sh
@@ -22,7 +22,8 @@ do
         Git)
             sudo apt-get install git
             git config --global user.email "zforster@hotmail.com"
-            git config --global user.name "Zach";; 
+            git config --global user.name "Zach"
+	    git config color.ui true;;
 
         Mercurial)
             sudo apt-get install mercurial
