443401671acbc7f3a778b30b794cd91a95c4e2ae
Zach <zforster@hotmail.com>
2013-11-18 13:47:28 -0600

    Finished script to install latest version of eclipse.

--- mint_init_setup.sh
+++ mint_init_setup.sh
@@ -53,17 +53,15 @@ do
             rm ~/Downloads/current_java.tar.gz;;
 
     'Eclipse') #TODO: Get this to work!
-            #cd ~/Downloads
-            #site_url=$(wget -q -O- http://eclipse.org/downloads/?osType=linux | grep -o -e 'www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/.*/eclipse-standard-.*-linux-gtk-x86_64.tar.gz')
-            #download_url=$(wget -q -0- $site_url | grep -o -e '$site_url&mirror_id=.*')
-            #wget $download_url -O 'current_eclipse.tar.gz'
-            #tar -zxvf current_eclipse.tar.gz
-            #rm current_eclipse.tar.gz
-            #sudo mv eclipse /opt
-            #cd /usr/local/bin
-            #sudo ln -s /opt/eclipse/eclipse
-            #cp /opt/eclipse/icon.xpm /usr/share/pixmaps/eclipse.xpm
-            ;;
+            cd ~/Downloads
+            file_url=$(wget -q -O- http://eclipse.org/downloads/?osType=linux | grep -o -e '/technology/epp/downloads/release/.*/eclipse-standard-.*-linux-gtk-x86_64.tar.gz')
+            wget http://download.eclipse.org$file_url -O 'current_eclipse.tar.gz'
+            tar -zxvf current_eclipse.tar.gz
+            rm current_eclipse.tar.gz
+            sudo mv eclipse /opt
+            cd /usr/local/bin
+            sudo ln -s /opt/eclipse/eclipse
+            cp /opt/eclipse/icon.xpm /usr/share/pixmaps/eclipse.xpm;;
         'Google Chrome')
             cd ~/Downloads
             sudo apt-get install libxss1
