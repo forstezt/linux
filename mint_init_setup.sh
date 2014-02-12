a67afe600f81381b008178eaa3a2a030ef658f22
Zach <zforster@hotmail.com>
2014-02-11 20:46:55 -0600

    added more error messages and checks for directories or files that already exist before creating them. mapping uwec drives doesn't seem to work in kubuntu.

--- mint_init_setup.sh
+++ mint_init_setup.sh
@@ -89,22 +89,37 @@ do
     case $yn in
         Yes)
             #if the .uwec_credentials file doesn't exits, create it
-            if [ ! -f ~/.uwec_credentials ]
-                then 
-                    touch ~/.uwec_credentials       
+            if [ ! -f ~/.uwec_credentials ]; then
+                touch ~/.uwec_credentials       
+                echo "~/.uwec_credentials file created"
+
+                #enter credentials in the .uwec_credentials file
+                echo "username=UWEC\\forstezt" >> ~/.uwec_credentials
+                echo "password=@FTZ#coconutpie" >> ~/.uwec_credentials
+                echo "uwec username and password written to file"
             fi
         
-            #enter credentials in the .uwec_credentials file
-            echo "username=UWEC\\forstezt" >> ~/.uwec_credentials
-            echo "password=@FTZ#coconutpie" >> ~/.uwec_credentials
 
-            #create folders to which to mount the h and w drives
-            sudo mkdir /media/H
-            sudo mkdir /media/W
+            #create folders to which the h and w drives will be mapped
+            if [ -d /media/H ]; then
+                echo "directory /media/H already exists"
+            else
+                sudo mkdir /media/H
+                echo "created directory /media/H"
+            fi
+
+            if [ -d /media/W ]; then
+                echo "directory /media/W already exists"
+            else
+                sudo mkdir /media/W
+                echo "created folder /media/W"
+            fi
 
             #enter mounting information into /etc/fstab
             sudo echo "//students.uwec.edu/forstezt\$ /media/H cifs credentials=/home/forstezt/.uwec_credentials,gid=1000,uid=1000,iocharset=utf8,sec=ntlm 0 0" >> /etc/fstab
+            echo "added line to /etc/fstab to map H: drive"
             sudo echo "//students.uwec.edu/deptdir /media/W cifs credentials=/home/forstezt/.uwec_credentials,gid=1000,uid=1000,iocharset=utf8,sec=ntlm 0 0" >> /etc/fstab;;
+            echo "added line to /etc/fstab to map W: drive"
 
         No)
             break;;
